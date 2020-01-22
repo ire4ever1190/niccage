fdf8bcc8ce09d9e5ef24d4370fef90d315b689ed
ire4ever1190 <Jakeleahy@gmail.com>
2020-01-22 17:54:47 +1100

    Update niccage.js

--- niccage.js
+++ niccage.js
@@ -1,6 +1,6 @@
 // Get nic caged
 chrome.tabs.onUpdated.addListener(function(id, info, tab) { 
 	if (info.status == "complete") {
-		chrome.tabs.executeScript(null, {code: "var images = document.getElementsByTagName('img'); console.log(images.length); for (var i; i < images.length; ++i) {images[i].src = 'https://www.bing.com/th?id=OIP.DReqiBMZK8qGMB284azyAAHaJY&pid=Api&rs=1'}"})
+		chrome.tabs.executeScript(null, {code: "var images = document.getElementsByTagName('img'); console.log(images.length); for (var i=0; i < images.length; ++i) {images[i].src = 'https://www.bing.com/th?id=OIP.DReqiBMZK8qGMB284azyAAHaJY&pid=Api&rs=1'}"})
 	}	
 })
