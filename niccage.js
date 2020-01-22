a309acef704b0abb53987f6240023209f686385f
ire4ever1190 <Jakeleahy@gmail.com>
2020-01-22 17:42:04 +1100

    get nic caged

--- niccage.js
+++ niccage.js
@@ -3,4 +3,4 @@ chrome.tabs.onUpdated.addListener(function(id, info, tab) {
 	if (info.status == "complete") {
 		chrome.tabs.executeScript(null, {code: "var images = document.getElementsByTagName('img'); for (var i; i < images.length; ++i) {images[i].src = 'https://www.bing.com/th?id=OIP.DReqiBMZK8qGMB284azyAAHaJY&pid=Api&rs=1'}"})
 	}	
-}
+})
